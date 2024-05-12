34900972a88e257a17ff46633bbed79a9f349e52
Mei Huang <meihuang8328@gmail.com>
2024-05-12 16:28:34 -0700

    added routes to app.js

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// const userRoutes = 
-// const showRoutes = 
+const userRoutes = require('./routes/userRoutes');
+const showRoutes = require('./routes/showRoutes');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/routes/users', userRoutes);
+app.use('/routes/shows', showRoutes);
 
 app.listen(PORT, () => {
     console.log(`Our server is now listening to port ${PORT}`)
